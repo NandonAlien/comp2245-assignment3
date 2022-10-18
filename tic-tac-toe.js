3036232178e72e8b9593c9df98b50e323d131151
Nandon <nandonarts@gmail.com>
2022-10-18 17:49:54 -0400

    making comments to the script

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,30 @@
 "use strict";
 
 //Board layout
+//Setting each div inside game board as a CSS class square
+/*Deciding whether to use classList property, setAttribute() method 
+    or className Property
+*/
+//Deciding to use onload or DOMContentLoader handler
 
 //X or O to the screen
+//Initializing Empty Array for tracking state of the game
+//use innerHTML or textContent property to make X or O show up
 
 //Changing styles using hover
 
 //Cheat Prevention
+//making the user not able to change the value of a square that 
+//already has X or O
+
 
 //Check for the winner
+//div with id = status is where the message should be placed in
+//  using innerHTML or textContent property to place in the status div
+//Adding class you-won to status div
+  
 
 //Restarting the game
-
+//clicks a new game button
+//checking for X and O in square for deletion
+//use click event
